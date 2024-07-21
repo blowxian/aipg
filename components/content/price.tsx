9a19f150dfd1aaea47ac7135ec9c12e71462e34b
lisonallen <lisonallen@qq.com>
2024-07-21 20:54:45 +0800

    remove dark mode on price section;

--- components/content/price.tsx
+++ components/content/price.tsx
@@ -15,10 +15,10 @@ const Price: React.FC = () => {
     })();
 
     return (
-        <section className="bg-white dark:bg-gray-900 px-3 md:px-0 !pb-3 !pt-8 md:!pb-8" id="price">
+        <section className="bg-white px-3 md:px-0 !pb-3 !pt-8 md:!pb-8" id="price">
             <div className="py-8 px-0 mx-auto max-w-screen-xl lg:py-16 lg:px-2">
                 <div className="mx-auto max-w-screen-md text-center mb-8 lg:mb-12">
-                    <h2 className="mt-3 text-xl md:text-3xl tracking-tight font-extrabold text-gray-900 dark:text-white">Choose
+                    <h2 className="mt-3 text-xl md:text-3xl tracking-tight font-extrabold text-gray-900">Choose
                         the
                         plan that suits your needs</h2>
                     <p className="mt-0 md:mt-2 !text-xs md:!text-xl !text-gray-500 !font-light !italic">Our plans are
@@ -145,22 +145,22 @@ const PlanCard: React.FC<{
 }> = ({title, price, originalPrice, duration, features, payLink, buttonText}) => {
     return (
         <div
-            className="flex flex-col p-3 mx-auto max-w-lg text-center text-gray-900 bg-white rounded-lg border border-gray-100 shadow dark:border-gray-600 xl:p-4 dark:bg-gray-800 dark:text-white">
+            className="flex flex-col p-3 mx-auto max-w-lg text-center text-gray-900 bg-white rounded-lg border border-gray-100 shadow xl:p-4">
             <h3 className="mb-4 text-2xl font-semibold">{title}</h3>
             <div className="flex justify-center items-baseline my-8">
                 {originalPrice && (
                     <span
-                        className="text-gray-500 dark:text-gray-400 line-through mr-1">{originalPrice}</span>
+                        className="text-gray-500 line-through mr-1">{originalPrice}</span>
                 )}
                 <span className="mr-2 text-5xl font-extrabold">{price}</span>
-                <span className="text-gray-500 dark:text-gray-400">{duration}</span>
+                <span className="text-gray-500">{duration}</span>
             </div>
             <Button size="3" asChild><a href={payLink}
                                         target={payLink !== "#" ? "_blank" : ""}>{buttonText}</a></Button>
             <ul role="list" className="mt-8 space-y-1 text-left text-xs">
                 {features.map((feature, index) => (
                     <li key={index} className="flex items-center space-x-3">
-                        <svg className="flex-shrink-0 w-5 h-5 text-green-500 dark:text-green-400" fill="currentColor"
+                        <svg className="flex-shrink-0 w-5 h-5 text-green-500" fill="currentColor"
                              viewBox="0 0 20 20" xmlns="http://www.w3.org/2000/svg">
                             <path fillRule="evenodd"
                                   d="M16.707 5.293a1 1 0 010 1.414l-8 8a1 1 0 01-1.414 0l-4-4a1 1 0 011.414-1.414L8 12.586l7.293-7.293a1 1 0 011.414 0z"
