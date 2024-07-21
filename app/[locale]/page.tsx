9a97367100a81ccdae6b0da24f44ec7dc0153802
lisonallen <lisonallen@qq.com>
2024-07-21 18:32:46 +0800

    fix layout;

--- app/[locale]/page.tsx
+++ app/[locale]/page.tsx
@@ -14,22 +14,25 @@ export default function Home() {
     const t = useTranslations('Index' as any);
 
     return (
-        <Container>
+        <>
             <Header/>
-            <Section id="generator" className="!pb-3 md:!pb-12">
-                <Heading as="h1" align="center" className="mt-3 md:mt-9 !text-xl md:!text-6xl">{t("title" as any)}</Heading>
-                <Heading as="h2" align="center"
-                         className="mt-0 md:mt-2 !text-xs md:!text-xl !text-gray-500 !font-light !italic">The Most
-                    Powerful AI Paragraph Generator Online</Heading>
-            </Section>
-            <ParagraphGenerator/>
-            <Scene/>
-            <Highlight/>
-            <Benefit/>
-            <Overcome/>
-            <FAQ/>
-            <Price/>
+            <Container>
+                <Section id="generator" className="!pb-3 md:!pb-12">
+                    <Heading as="h1" align="center"
+                             className="mt-3 md:mt-9 !text-xl md:!text-6xl">{t("title" as any)}</Heading>
+                    <Heading as="h2" align="center"
+                             className="mt-0 md:mt-2 !text-xs md:!text-xl !text-gray-500 !font-light !italic">The Most
+                        Powerful AI Paragraph Generator Online</Heading>
+                </Section>
+                <ParagraphGenerator/>
+                <Scene/>
+                <Highlight/>
+                <Benefit/>
+                <Overcome/>
+                <FAQ/>
+                <Price/>
+            </Container>
             <Footer border={true}/>
-        </Container>
+        </>
     );
 }
