aa6df994ca99e69921b36dc895d73556714417c0
lisonallen <lisonallen@qq.com>
2024-07-23 12:33:39 +0800

    fix price card width;

--- components/content/price.tsx
+++ components/content/price.tsx
@@ -4,6 +4,7 @@ import {Button} from '@radix-ui/themes';
 import {useLocale, useMessages, useTranslations} from 'next-intl';
 import Cookies from 'js-cookie';
 import {useState} from 'react';
+import {CheckIcon, Cross2Icon, RocketIcon} from "@radix-ui/react-icons";
 
 const Price: React.FC = () => {
     const t = useTranslations('Price');
@@ -72,7 +73,7 @@ const PlanCard: React.FC<{
 }> = ({title, price, originalPrice, duration, features, handlePayLinkClick, buttonText}) => {
     return (
         <div
-            className="flex flex-col p-3 mx-auto max-w-lg text-center text-gray-900 bg-white rounded-lg border border-gray-100 shadow xl:p-4">
+            className="flex flex-col p-3 mx-auto w-full max-w-lg text-center text-gray-900 bg-white rounded-lg border border-gray-100 shadow xl:p-4">
             <h3 className="mb-4 text-2xl font-semibold">{title}</h3>
             <div className="flex justify-center items-baseline my-8">
                 {originalPrice && (
@@ -85,12 +86,15 @@ const PlanCard: React.FC<{
             <ul role="list" className="mt-8 space-y-1 text-left text-xs">
                 {features.map((feature, index) => (
                     <li key={index} className="flex items-center space-x-3">
-                        <svg className="flex-shrink-0 w-5 h-5 text-green-500" fill="currentColor" viewBox="0 0 20 20"
+                        {/*<svg className="flex-shrink-0 w-5 h-5 text-green-500" fill="currentColor" viewBox="0 0 20 20"
                              xmlns="http://www.w3.org/2000/svg">
                             <path fillRule="evenodd"
                                   d="M16.707 5.293a1 1 0 010 1.414l-8 8a1 1 0 01-1.414 0l-4-4a1 1 0 011.414-1.414L8 12.586l7.293-7.293a1 1 0 011.414 0z"
                                   clipRule="evenodd"></path>
-                        </svg>
+                        </svg>*/}
+                        <CheckIcon className={'text-green-500'}/>
+                        <Cross2Icon className={'text-red-500'}/>
+                        <RocketIcon className={'text-orange-500'}/>
                         <span>{feature}</span>
                     </li>
                 ))}
