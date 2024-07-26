ed5cebe3368bda64c77bb80da2e4e984905cf446
lisonallen <lisonallen@qq.com>
2024-07-26 13:29:13 +0800

    impl lang switcher on footer;

--- components/language-switcher.tsx
+++ components/language-switcher.tsx
@@ -24,6 +24,34 @@ const languages = [
     // Add more languages here
 ];
 
+interface SelectItemProps {
+    children: React.ReactNode;
+    value: string;
+    isSelected: boolean;
+}
+
+const SelectItem = React.forwardRef<HTMLDivElement, SelectItemProps>(
+    ({children, isSelected, ...props}, forwardedRef) => {
+        return (
+            <Select.Item
+                className={classnames(
+                    'text-[13px] leading-none text-violet11 rounded-[3px] flex items-center h-[25px] pr-[35px] pl-[25px] relative select-none data-[disabled]:text-mauve8 data-[disabled]:pointer-events-none data-[highlighted]:outline-none data-[highlighted]:bg-violet9 data-[highlighted]:text-violet1',
+                    {'data-[highlighted]:bg-violet9': isSelected}
+                )}
+                {...props}
+                ref={forwardedRef}
+            >
+                <Select.ItemText>{children}</Select.ItemText>
+                <Select.ItemIndicator className="absolute left-0 w-[25px] inline-flex items-center justify-center">
+                    <CheckIcon/>
+                </Select.ItemIndicator>
+            </Select.Item>
+        );
+    }
+);
+
+SelectItem.displayName = 'SelectItem';
+
 const LanguageSwitcher = () => {
     const currentLocale = useLocale();
     const router = useRouter();
@@ -33,7 +61,7 @@ const LanguageSwitcher = () => {
     };
 
     return (
-        <Select.Root className="z-10" defaultValue={currentLocale} onValueChange={handleLanguageChange}>
+        <Select.Root defaultValue={currentLocale} onValueChange={handleLanguageChange}>
             <Select.Trigger
                 className="inline-flex items-center justify-center rounded px-[15px] text-[13px] leading-none h-[35px] gap-[5px] bg-white text-violet11 shadow-[0_0_2px] shadow-black/10 hover:bg-mauve3 focus:shadow-[0_0_0_2px] focus:shadow-black data-[placeholder]:text-violet9 outline-none"
                 aria-label="Language"
@@ -71,24 +99,5 @@ const LanguageSwitcher = () => {
         </Select.Root>
     );
 };
-
-const SelectItem = React.forwardRef(({children, className, isSelected, ...props}, forwardedRef) => {
-    return (
-        <Select.Item
-            className={classnames(
-                'text-[13px] leading-none text-violet11 rounded-[3px] flex items-center h-[25px] pr-[35px] pl-[25px] relative select-none data-[disabled]:text-mauve8 data-[disabled]:pointer-events-none data-[highlighted]:outline-none data-[highlighted]:bg-violet9 data-[highlighted]:text-violet1',
-                className,
-                {'data-[highlighted]:bg-violet9': isSelected}
-            )}
-            {...props}
-            ref={forwardedRef}
-        >
-            <Select.ItemText>{children}</Select.ItemText>
-            <Select.ItemIndicator className="absolute left-0 w-[25px] inline-flex items-center justify-center">
-                <CheckIcon/>
-            </Select.ItemIndicator>
-        </Select.Item>
-    );
-});
 
 export default LanguageSwitcher;
--- components/ui/footer.tsx
+++ components/ui/footer.tsx
@@ -17,7 +17,7 @@ export default function Footer({border = false}: { border?: boolean }) {
                     className={`grid gap-10 py-8 sm:grid-cols-12 md:py-12 ${border ? "border-t [border-image:linear-gradient(to_right,transparent,theme(colors.slate.200),transparent)1]" : ""}`}
                 >
                     {/* 1st block */}
-                    <div className="space-y-2 sm:col-span-12 lg:col-span-4">
+                    <div className="space-y-2 sm:col-span-12 lg:col-span-4 z-10">
                         <div>
                             <Logo/>
                         </div>
